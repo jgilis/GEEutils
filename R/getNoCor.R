8fc1d2e5dad6effeeea1b7368a9bbc676e1f1d7f
Milan Malfait <milan.malfait94@gmail.com>
2021-03-02 17:47:00 +0100

    Update `getNoCor()`
    - Remove `as.data.frame` conversions
    - Pre-allocate `selected_genes` vector

--- R/getNoCor.R
+++ R/getNoCor.R
@@ -38,13 +38,12 @@ getNoCor <- function(object,
                      nFeatures = 500,
                      method = "spearman",
                      cutoff = 0.25) {
-    # TODO: remove `as.data.frame` conversions, cor works with matrices
     if (is(object, "matrix")) {
-        counts <- as.data.frame(t(object)) # transpose necessary for cor function
+        counts <- t(object) # transpose necessary for cor function
     } else if (is(object, "SummarizedExperiment")) {
         stopifnot("counts" %in% assayNames(object))
         counts <- assays(object)[["counts"]]
-        counts <- as.data.frame(t(as.matrix(counts))) # transpose necessary for cor function
+        counts <- t(as.matrix(counts)) # transpose necessary for cor function
     } else {
         stop(
           "The provided object is not a matrix nor a SingleCellExperiment",
@@ -53,14 +52,16 @@ getNoCor <- function(object,
     }
 
     all_corr <- cor(counts, method = method) # compute all pairwise correlations, so gene x gene matrix. If too big, one code sample 1 by 1 as Zimmerman.
-    selected_genes <- c() # FIXME: growing vector very inefficient; better way?
+    selected_genes <- character(length = nFeatures)
 
     for (i in seq_len(nFeatures)) {
         current_gene <- sample(colnames(all_corr), 1)
-        selected_genes <- c(selected_genes, current_gene)
+        selected_genes[[i]] <- current_gene
         remove <- which(abs(all_corr[current_gene, ]) > cutoff)
         all_corr <- all_corr[-remove, -remove]
         if (ncol(all_corr) < 10) break # if too few uncorrelated features left --> break
     }
+    ## Remove empty strings in selected_genes
+    selected_genes <- selected_genes[nzchar(selected_genes)]
     object[selected_genes, ]
 }
