c99244470b5963c2e90f27f9766c2d77ebdee250
Milan Malfait <milan.malfait94@gmail.com>
2021-04-26 18:57:30 +0200

    Add unit tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(GEEutils)
+
+test_check("GEEutils")
---
+++ tests/testthat/test-sandwich.R
@@ -0,0 +1,62 @@
+## Mock up data
+set.seed(42)
+n_cells <- 50L
+n_genes <- 100L
+sce <- scuttle::mockSCE(ncells = n_cells, ngenes = n_genes)
+subject_id <- "Cell_Cycle"
+model_fits <- fitGLM(sce, ~ Mutation_Status + Treatment)
+
+test_that("Vanilla glmSandwichTest() works", {
+    ## Uses 'Li-Redden' adjustment by default
+    out <- glmSandwichTest(model_fits, subject_id = subject_id, coef = 3)
+
+    pars <- out$params
+    expect_identical(pars$type, "LiRedden")
+    expect_identical(pars$subject_id, subject_id)
+    expect_false(pars$cadjust)
+    expect_false(pars$fix)
+
+    tab <- out$table
+    expect_s3_class(tab, "data.frame")
+    expect_identical(nrow(tab), length(model_fits))
+})
+
+test_that("glmSandwichTest() works with single model fit", {
+    m <- model_fits[[1]]
+    out <- glmSandwichTest(m, subject_id = subject_id, coef = 3)
+    tab <- out$table
+    expect_s3_class(tab, "data.frame")
+    expect_identical(nrow(tab), 1L)
+})
+
+test_that("glmSandwichTest() works with contrasts", {
+    contr <- c(-1, 0, 1)
+    out <- glmSandwichTest(model_fits, subject_id = subject_id, contrast = contr)
+    tab <- out$table
+    expect_s3_class(tab, "data.frame")
+    expect_identical(nrow(tab), length(model_fits))
+})
+
+test_that("glmSandwichTest() fails and warns when necessary", {
+    expect_error(
+        glmSandwichTest(model_fits, subject_id = subject_id, coef = "random")
+    )
+    expect_error(
+        glmSandwichTest(model_fits, subject_id = subjet_id, coef = 1e5)
+    )
+    expect_error(
+        glmSandwichTest(model_fits, subject_id = subject_id, coef = TRUE)
+    )
+    contr <-  cbind(1:3, 4:6)
+    expect_error(
+        glmSandwichTest(model_fits, subject_id = subject_id, contrast = contr)
+    )
+    expect_error(glmSandwichTest(model_fits, subject_id = subjet_id))
+    expect_error(glmSandwichTest(model_fits, subject_id = "bla", coef = 3))
+    expect_warning(
+        out <- glmSandwichTest(
+            model_fits, subject_id = NULL, coef = 3, type = "LiRedden"
+        )
+    )
+    expect_identical(out$params$type, "HC0")
+})
