43cf8094730d3708ea3f1ff8e5ec5efc4e745296
Milan Malfait <milan.malfait94@gmail.com>
2021-05-03 16:19:40 +0200

    Refactor `filterBySample()`
    Simplify code:
    
    - Remove `group_id`: not necessary if we already filter at the sample-level
    - Remove convoluted `map_depth()` calls; not necessary anymore since we just split by a single level (samples)
    - Remove `.split_cells_filter()` helper: can be done much simpler
    - Rename `assay` argument to `use_assay` (to avoid confusion with `assay()` function)
    - Only pass around relevant parts of the SCE object to helpers
    - Remove spurious dependencies

--- R/filterBySample.R
+++ R/filterBySample.R
@@ -7,10 +7,6 @@
 #'
 #' @param object A `SingleCellExperiment` object
 #'
-#' @param group_id A `vector` which identifies the experimental groups of
-#' interest. Must be one of the column names in the `colData` of the provided
-#' `SingleCellExperiment` object
-#'
 #' @param sample_id A `vector` which identifies the samples or experimental
 #' units. Must be one of the column names in the `colData` of the provided
 #' `SingleCellExperiment` object
@@ -18,9 +14,9 @@
 #' @param size A `number` defining the minimum amount of samples that must
 #' express a gene for the gene to pass the filtering. Defaults to 2.
 #'
-#' @param assay A `character` specifying which `assay` from the
-#' `SingleCellExperiment` to use. If `NULL` will use the first one in
-#' `assayNames(object)`.
+#' @param use_assay A `character` or `integer` specifying which `assay` from the
+#' `SingleCellExperiment` to use. Default: `1L`, which will use the first assay
+#' in `assayNames(object)`.
 #'
 #' @return An updated `SingleCellExperiment` object, with all genes that did
 #' not pass the filtering removed.
@@ -32,98 +28,39 @@
 #' set.seed(011235)
 #' sce <- mockSCE(ncells = 400, ngenes = 200)
 #' sce$patient_id <- factor(rep(paste0("patient", 1:8), each = ncol(sce) / 8))
-#' sce$group_id <- factor(rep(paste0("group", 1:2), each = ncol(sce) / 2))
 #'
 #' ## Set some genes to 0 for all but 1 patient
 #' counts(sce)[1:10, -which(sce$patient_id == "patient1")] <- 0
 #'
-#' sce_filt <- filterBySample(
-#'     object = sce,
-#'     group_id = "group_id",
-#'     sample_id = "patient_id",
-#'     size = 2,
-#'     assay = NULL
-#' )
+#' sce_filt <- filterBySample(sce, sample_id = "patient_id", size = 2)
 #' sce_filt # filtered SCE object
 #'
 #' @export
-filterBySample <- function(object, group_id, sample_id,
-                           size = 2, assay = NULL) {
-    if (is.null(assay)) {
-        assay <- assayNames(object)[[1]]
-    }
+filterBySample <- function(object, sample_id,
+                           size = 2, use_assay = 1L) {
 
-    object_binary <- object # make copy
-    assays(object_binary)[[assay]][assays(object_binary)[[assay]] > 0] <- 1
-    # set counts > 0 to 1 -> upon aggregation, we count the number of cells
+    # Binarize assay -> upon aggregation, we count the number of cells
     # with any expression for each gene
+    binary_assay <- assay(object, use_assay) > 0
 
-    cs <- .split_cells_filter(
-        x = object_binary,
-        by = c(group_id, sample_id)
-    )
+    samples <- factor(object[[sample_id]])
+    split_cells <- split(seq_len(ncol(object)), f = samples, drop = TRUE)
 
-    pb <- .pb_filter(
-        x = object_binary,
-        cs = cs,
-        assay = assay
-    )
-
-    remove <- c()
-    for (i in seq_along(pb)) { # loop over group_id
-        # remove genes if less than "size" samples have expression in
-        # a certain group
-        remove <- c(names(which(rowSums(pb[[i]] > 0) <= size)), remove)
-        # NOTE: the >0 could also be >count, with count an input of filterBySample
-    }
-
-    if (length(remove) == 0) {
-        return(object)
-    } else {
-        return(object[-which(rownames(object) %in% unique(remove)), ])
-    }
+    pb <- .counts_per_sample(x = binary_assay, split_cells = split_cells)
+    keep <- rowSums(pb > 0) > size
+    # NOTE: the >0 could also be >count, with count an input of filterBySample
+    object[keep, ]
 }
 
 
-# Adapted from Muscat
-# See: https://github.com/HelenaLC/muscat/, utils.R
-# ------------------------------------------------------------------------------
-# split cells by group-sample
-# ------------------------------------------------------------------------------
-#   x:  a SingleCellExperiment
-#   by: character vector specifying colData column(s) to split by
-# ------------------------------------------------------------------------------
-#' @importFrom data.table data.table
-#' @importFrom purrr map_depth
-.split_cells_filter <- function(x, by) {
-    if (is(x, "SingleCellExperiment")) x <- colData(x)
-    cd <- data.frame(x[by], check.names = FALSE)
-    cd <- data.table(cd, cell = rownames(x))
-    cd <- split(cd, by = by, sorted = TRUE, flatten = FALSE, drop = TRUE)
-    # added drop=TRUE in split to remove empty group*sample combinations
-    map_depth(cd, length(by), "cell")
-}
-
-# Adapted from Muscat
-# See: https://github.com/HelenaLC/muscat/, utils-pbDS.R
-# ------------------------------------------------------------------------------
-# Aggregate cells that originate from the same group-sample combination
-# ------------------------------------------------------------------------------
-#   x:  a SingleCellExperiment
-#   cs: character vector specifying colData column(s) to split by
-#   assay: defaults to first assay of the SingleCellExperiment
-# ------------------------------------------------------------------------------
 #' @importFrom Matrix rowSums
-#' @importFrom purrr map_depth
-.pb_filter <- function(x, cs, assay) {
-    pb <- map_depth(cs, -1, function(i) {
+.counts_per_sample <- function(x, split_cells) {
+    out <- lapply(split_cells, function(i) {
         if (length(i) == 0) {
             return(numeric(nrow(x)))
         }
-        rowSums(assays(x)[[assay]][, i, drop = FALSE])
+        rowSums(x[, i, drop = FALSE])
         # set aggregation function to Matrix::rowSums <-> muscat
     })
-    map_depth(pb, -2, function(u) {
-        as.matrix(data.frame(u, row.names = rownames(x), check.names = FALSE))
-    })
+    do.call(cbind, out)
 }
