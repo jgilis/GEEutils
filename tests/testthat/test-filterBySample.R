5b30dc900f639b42d32b37f332d87356fc5c29fd
Milan Malfait <milan.malfait94@gmail.com>
2021-05-06 17:39:48 +0200

    Use 'subject' instead of 'patient' in tests

--- tests/testthat/test-filterBySample.R
+++ tests/testthat/test-filterBySample.R
@@ -5,16 +5,16 @@ set.seed(42)
 n_cells <- 200L
 n_genes <- 100L
 ref <- mockSCE(ncells = n_cells, ngenes = n_genes)
-ref$patient_id <- factor(rep(paste0("patient", 1:8), each = ncol(ref) / 8))
+ref$subject_id <- rep(paste0("patient", 1:8), each = ncol(ref) / 8)
 
 test_that("filterBySample() works", {
     n_drop <- 10L # number of genes that should be removed
 
     ## Set some genes to 0 for all but 1 patient
-    counts(ref)[seq_len(n_drop), -which(ref$patient_id == "patient1")] <- 0
+    counts(ref)[seq_len(n_drop), -which(ref$subject_id == "patient1")] <- 0
     to_drop <- rownames(ref)[seq_len(n_drop)]
 
-    out <- filterBySample(ref, sample_id = "patient_id", size = 2)
+    out <- filterBySample(ref, sample_id = "subject_id", size = 2)
     expect_equal(nrow(out), nrow(ref) - n_drop)
     expect_false(any(to_drop %in% rownames(out)))
 })
--- tests/testthat/test-sandwich.R
+++ tests/testthat/test-sandwich.R
@@ -2,17 +2,19 @@
 set.seed(42)
 n_cells <- 50L
 n_genes <- 100L
+n_subjects <- 5L
+
 sce <- scuttle::mockSCE(ncells = n_cells, ngenes = n_genes)
-subject_id <- "Cell_Cycle"
+sce$subject_id <- rep(paste0("subject", 1:n_subjects), each = ncol(sce) / n_subjects)
 model_fits <- fitGLM(sce, ~ Mutation_Status + Treatment)
 
 test_that("Vanilla glmSandwichTest() works", {
     ## Uses 'Li-Redden' adjustment by default
-    out <- glmSandwichTest(model_fits, subject_id = subject_id, coef = 3)
+    out <- glmSandwichTest(model_fits, subject_id = "subject_id", coef = 3)
 
     pars <- out$params
     expect_identical(pars$type, "LiRedden")
-    expect_identical(pars$subject_id, subject_id)
+    expect_identical(pars$subject_id, "subject_id")
     expect_false(pars$cadjust)
     expect_false(pars$fix)
 
@@ -23,7 +25,7 @@ test_that("Vanilla glmSandwichTest() works", {
 
 test_that("glmSandwichTest() works with single model fit", {
     m <- model_fits[[1]]
-    out <- glmSandwichTest(m, subject_id = subject_id, coef = 3)
+    out <- glmSandwichTest(m, subject_id = "subject_id", coef = 3)
     tab <- out$table
     expect_s3_class(tab, "data.frame")
     expect_identical(nrow(tab), 1L)
@@ -31,7 +33,7 @@ test_that("glmSandwichTest() works with single model fit", {
 
 test_that("glmSandwichTest() works with contrasts", {
     contr <- c(-1, 0, 1)
-    out <- glmSandwichTest(model_fits, subject_id = subject_id, contrast = contr)
+    out <- glmSandwichTest(model_fits, subject_id = "subject_id", contrast = contr)
     tab <- out$table
     expect_s3_class(tab, "data.frame")
     expect_identical(nrow(tab), length(model_fits))
@@ -39,19 +41,19 @@ test_that("glmSandwichTest() works with contrasts", {
 
 test_that("glmSandwichTest() fails and warns when necessary", {
     expect_error(
-        glmSandwichTest(model_fits, subject_id = subject_id, coef = "random")
+        glmSandwichTest(model_fits, subject_id = "subject_id", coef = "random")
     )
     expect_error(
-        glmSandwichTest(model_fits, subject_id = subjet_id, coef = 1e5)
+        glmSandwichTest(model_fits, subject_id = "subject_id", coef = 1e5)
     )
     expect_error(
-        glmSandwichTest(model_fits, subject_id = subject_id, coef = TRUE)
+        glmSandwichTest(model_fits, subject_id = "subject_id", coef = TRUE)
     )
     contr <-  cbind(1:3, 4:6)
     expect_error(
-        glmSandwichTest(model_fits, subject_id = subject_id, contrast = contr)
+        glmSandwichTest(model_fits, subject_id = "subject_id", contrast = contr)
     )
-    expect_error(glmSandwichTest(model_fits, subject_id = subjet_id))
+    expect_error(glmSandwichTest(model_fits, subject_id = "subject_id"))
     expect_error(glmSandwichTest(model_fits, subject_id = "bla", coef = 3))
     expect_warning(
         out <- glmSandwichTest(
